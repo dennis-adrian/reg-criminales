904be498ebd0a04a269bfd4b79f7c9c5d98b3795
dennis adrian <adriangbetancur@gmail.com>
2020-10-16 16:53:34 -0400

    Updates

---
+++ precache-manifest.24763021418d2d4518d304e693fb08b6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81cd446c42bff91ccd9124c7f46c8b25",
+    "url": "/reg-criminales/index.html"
+  },
+  {
+    "revision": "de4005587a214fae6c11",
+    "url": "/reg-criminales/static/css/main.6999c77c.chunk.css"
+  },
+  {
+    "revision": "f3cf6a464dcfca21c3d5",
+    "url": "/reg-criminales/static/js/2.0af42879.chunk.js"
+  },
+  {
+    "revision": "d603a6d29bea01972f62",
+    "url": "/reg-criminales/static/js/3.0cdab1c6.chunk.js"
+  },
+  {
+    "revision": "016aa54dbb17435300cc2a91d7189edb",
+    "url": "/reg-criminales/static/js/3.0cdab1c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "374c5161809385c9562d",
+    "url": "/reg-criminales/static/js/4.acf08330.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/reg-criminales/static/js/4.acf08330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7644e18f67b631b3825b",
+    "url": "/reg-criminales/static/js/5.b5405e20.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/reg-criminales/static/js/5.b5405e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4005587a214fae6c11",
+    "url": "/reg-criminales/static/js/main.dfda5b79.chunk.js"
+  },
+  {
+    "revision": "c4927c1b92503b63e948",
+    "url": "/reg-criminales/static/js/runtime-main.12d86e7b.js"
+  }
+]);
